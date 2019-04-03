2686da2835df45eac455871fac8a6b38eec3376d
Alex <ale8under8@gmail.com>
2019-04-02 18:13:28 -0700

    add buttons styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Input from './components/Input.jsx';
+import Button from './components/Button';
+// import ClearButton from './components/ClearButton.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+        </div>
       </div>
     );
   }
