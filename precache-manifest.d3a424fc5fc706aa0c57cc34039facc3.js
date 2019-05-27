462367ae1686b3ea6c87b00fdbd1e99bbfbc9d60
Alex <ale8under8@gmail.com>
2019-05-27 16:10:32 -0700

    Updates

---
+++ precache-manifest.d3a424fc5fc706aa0c57cc34039facc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/React-Calculator/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  },
+  {
+    "revision": "a30fe6293d4f74493cc4",
+    "url": "/React-Calculator/static/js/main.e8a4f00a.chunk.js"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/React-Calculator/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "a30fe6293d4f74493cc4",
+    "url": "/React-Calculator/static/css/main.efdd84b9.chunk.css"
+  },
+  {
+    "revision": "3275aa79ce185b05bb646ca2240e26dc",
+    "url": "/React-Calculator/index.html"
+  }
+];
